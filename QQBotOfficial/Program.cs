e70ba1761624455811240e4da75c37a8323352b4
钱爽 <1969154690@qq.com>
2025-08-28 19:41:32 +0800

    更新介绍网页，添加头像

--- QQBotOfficial/Program.cs
+++ QQBotOfficial/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.Extensions.FileProviders;
 using QQBotOfficial;
 
 public class Program
@@ -13,11 +14,18 @@ public class Program
         Console.WriteLine(TokenManager.AccessToken);
         var builder = WebApplication.CreateBuilder(args);
 
+
         // 配置 Kestrel 监听 8443 端口，启用 HTTPS
         builder.WebHost.ConfigureKestrel(options => { options.ListenAnyIP(8443, listenOptions => { }); });
 
         var app = builder.Build();
-
+        
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "img")),
+            RequestPath = "/img"
+        });
         // WebHook 接收端点 - 使用正确的ASP.NT Core方式
         app.MapPost("/qqbotofficial/api", async (HttpContext context) => await PostQqBotOfficial.PostHandler(context));
         app.MapGet("/", async (HttpContext context) => await GetQqBotOfficial.GetHandler(context));
