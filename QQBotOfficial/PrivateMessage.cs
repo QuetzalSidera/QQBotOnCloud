ad345affdf8fa900d9a96842247d1e2978212952
钱爽 <1969154690@qq.com>
2025-08-28 08:19:11 +0800

    fix bugs

--- QQBotOfficial/PrivateMessage.cs
+++ QQBotOfficial/PrivateMessage.cs
@@ -13,29 +13,36 @@ public static class PrivateMessage
     {
         try
         {
+            Console.WriteLine("in PrivateMessage Handler 1");    
             var response = JsonSerializer.Deserialize<EventPayload<PrivateReceiveMessage>>(body);
+            Console.WriteLine("in PrivateMessage Handler 2");    
             if (response == null)
                 return;
-
+            Console.WriteLine("in PrivateMessage Handler 3");    
             //生成唯一ID
             var id = body.GetContextId();
             if (id == null)
                 return;
+            Console.WriteLine("in PrivateMessage Handler 4");    
             //获取用户标识
             var openId = response.Data.Author.OpenId;
             var name = response.Data.Author.OpenId;
             var message = response.Data.Content;
+            Console.WriteLine("in PrivateMessage Handler 5");    
             //如果普通命令没有处理，则交由AI
             if (!(await Commands.Handler(body, ChatType.Private)))
             {
+                Console.WriteLine("in PrivateMessage Handler 6");    
                 var result = await Models.DeepSeek.SendRequest(id, name, message);
                 await Tools.SendPrivateMessage(result, openId);
             }
+            Console.WriteLine("in PrivateMessage Handler 7");    
         }
         catch (Exception ex)
         {
             Console.WriteLine("私聊消息处理错误");
             Console.WriteLine(body);
+            
             return;
         }
     }
